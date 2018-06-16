b0752895408ad30a54c5e72fe5c330c32bbfce2a
PLorentz <PLorentz@users.noreply.github.com>
2018-06-16 15:28:00 -0300

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Pedro Lorentz - Azure CSE Team
+        Pedro Lorentz - CSE LATAM
       </div>
       <div class="col-lg-6">
       </div>
